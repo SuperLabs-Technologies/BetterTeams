8c4c285e9d3e3db55bedcc7b857ccfa2dbff30c2
NativeX64 <57600814+Draugr-official@users.noreply.github.com>
2022-09-08 20:58:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Initialize client */
 function InitializeUI() {
-    var extensionsBarElement = document.querySelector('[title="Meldingstillegg"]').parentElement;
+    var extensionsBarElement = document.querySelector('[data-tid="newMessageCommands-expand-compose"]').parentElement;
     var extensionButton = document.createElement('button');
     extensionsBarElement.insertAdjacentElement('afterbegin', extensionButton);
     extensionButton.outerHTML = '<button title="Manage extensions" onclick="ToggleUIStatus()" style="cursor: pointer; border: solid transparent; background-color: transparent; display: flex; justify-content: center; align-items: center; padding-top: 2px;" width="32" height="32"> <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="14" height="14"> <path d="M7,0H4A4,4,0,0,0,0,4V7a4,4,0,0,0,4,4H7a4,4,0,0,0,4-4V4A4,4,0,0,0,7,0ZM9,7A2,2,0,0,1,7,9H4A2,2,0,0,1,2,7V4A2,2,0,0,1,4,2H7A2,2,0,0,1,9,4Z" fill="#424242"/> <path d="M7,13H4a4,4,0,0,0-4,4v3a4,4,0,0,0,4,4H7a4,4,0,0,0,4-4V17A4,4,0,0,0,7,13Zm2,7a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V17a2,2,0,0,1,2-2H7a2,2,0,0,1,2,2Z" fill="#424242"/> <path d="M20,13H17a4,4,0,0,0-4,4v3a4,4,0,0,0,4,4h3a4,4,0,0,0,4-4V17A4,4,0,0,0,20,13Zm2,7a2,2,0,0,1-2,2H17a2,2,0,0,1-2-2V17a2,2,0,0,1,2-2h3a2,2,0,0,1,2,2Z" fill="#424242"/> <path d="M14,7h3v3a1,1,0,0,0,2,0V7h3a1,1,0,0,0,0-2H19V2a1,1,0,0,0-2,0V5H14a1,1,0,0,0,0,2Z" fill="#424242"/> </svg></button>';
@@ -21,7 +21,7 @@ function ToggleUIStatus() {
 
 /* RELEASE */
 var chatBoxElement = document.querySelector(
-    'div[aria-placeholder="Skriv en ny melding"]',
+    'div[data-tid="ckeditor"]',
 );
 chatBoxElement.innerHTML = ""; // Converting body to text
 // https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json
